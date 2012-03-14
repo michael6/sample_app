ad54c7e3c2fcf4dc4b466aaa560c47178ee8669a
Shaofen CHEN <gretchendx@live.com>
2012-03-13 20:49:12 -0700

    Exercise5.2 commit

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -31,4 +31,22 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  
+  #------------------------
+  
+    it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")# fill in
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")# fill in
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")# fill in
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")# fill in
+  end
+  
+  
 end
