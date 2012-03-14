91feab7d46cb3da38fbf2d0d212861cccd5bf98d
michael6 <michaelf614@gmail.com>
2012-03-13 21:09:16 -0700

    change

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -32,28 +32,15 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
   
-<<<<<<< HEAD
-  it "should have the right links on the layout" do
-=======
+
   #------------------------
   
     it "should have the right links on the layout" do
->>>>>>> shaofenchen/exercise5.2
+
     visit root_path
     click_link "About"
     response.should have_selector('title', :content => "About")
     click_link "Help"
-<<<<<<< HEAD
-    response.should have_selector('title', :content => "Help")
-    click_link "Contact"
-    response.should have_selector('title', :content => "Contact")
-    click_link "Home"
-    response.should have_selector('title', :content => "Home")
-    click_link "Sign up now!"
-    response.should have_selector('title', :content => "Sign up")
-  end
-  
-=======
     response.should have_selector('title', :content => "Help")# fill in
     click_link "Contact"
     response.should have_selector('title', :content => "Contact")# fill in
@@ -63,6 +50,4 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up")# fill in
   end
   
-  
->>>>>>> shaofenchen/exercise5.2
 end
