aa7ff379db2993802cdd3262255ebcd3b32e7196
michael6 <michaelf614@gmail.com>
2012-03-13 20:38:43 -0700

    done exc5.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def logo
+  
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+  def title
+     base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
