d445b9fa033d1a9375a4bd0dbc0605f409cd54a4
michael6 <michaelf614@gmail.com>
2012-04-08 17:22:10 -0700

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,49 @@
 #  updated_at :datetime
 #
 
+require 'digest'
 class User < ActiveRecord::Base
-   attr_accessible :name, :email
+   attr_accessor :password
+   attr_accessible :name, :email, :password, :password_confirmation
    
    validates :name, :presence => true
    validates :email, :presence => true
+   
+   # Automatically create the virtual attribute 'password_confirmation'.
+   validates :password, :presence     => true,
+                        :confirmation => true,
+                        :length       => { :within => 6..40 }
+						
+   before_save :encrypt_password
+   
+   # Return true if the user's password matches the submitted password.
+   def has_password?(submitted_password)
+     encrypted_password == encrypt(submitted_password)
+     
+   end
+  
+   def self.authenticate(email, submitted_password)
+     user = find_by_email(email)
+     return nil  if user.nil?
+     return user if user.has_password?(submitted_password)
+   end
+  
+   private
+
+    def encrypt_password
+	  self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash ("#{salt} -- #{string}")
+    end
+	
+	def make_salt
+	  secure_hash ("#{Time.now.utc}--#{password}")
+	end
+	
+	def secure_hash(string) 
+	   Digest::SHA2.hexdigest(string)
+	end
 end
